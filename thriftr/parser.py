370a96bfaab3e947576a3806bc29d44a507e4423
hit9 <nz2324@126.com>
2014-09-01 21:18:28 +0800

    full grammer

--- thriftr/parser.py
+++ thriftr/parser.py
@@ -2,85 +2,184 @@
 # ref: https://thrift.apache.org/docs/idl
 
 from ply import yacc
-from . import ThriftSyntaxError, ThriftUnkownType
+from . import ThriftSyntaxError
 from .lexer import tokens
 
-
 dct = {
-    'includes': [],
-    'namespaces': [],
-    'consts': {},
-    'typedefs': [],
+    'include': [],
+    'namespace': [],
+    'const': {},
+    'typedef': {},
 }
-
-
-def parse(data):
-    for key in dct:
-        dct[key] = []
-    parser.parse(data)
-    return dct
-
-
-converters = {
-    'bool': bool,
-    'byte': int,
-    'i16': int,
-    'i32': int,
-    'i64': int,
-    'double': float,
-    'string': str,
-    'binary': str,
-}
-
-
-# cast thrift values to python values
-def cast(val, tp):
-    if isinstance(tp, str) and tp in converters:
-        return converters[tp](val)
-    elif isinstance(tp, list):
-        if tp[0] == 'list':
-            t = tp[1]
-            return map(lambda v: cast(v, t), val)
-        elif tp[0] == 'map':
-            kt = tp[1]
-            vt = tp[2]
-            d = {}
-            for k, v in d.items():
-                d[cast(k, kt)] = cast(v, vt)
-            return d
-    raise ThriftUnkownType(tp)
-
 
 def p_error(p):
     raise ThriftSyntaxError(p)
 
 
 def p_start(p):
-    '''start : unit start
-             | '''
-    pass
+    '''start : header definition'''
 
 
-def p_unit(p):
-    '''unit : include
-            | const'''
+def p_header(p):
+    '''header : header_unit header
+              |'''
+
+def p_header_unit(p):
+    '''header_unit : include
+                   | namespace'''
 
 
 def p_include(p):
     '''include : INCLUDE LITERAL'''
-    dct['includes'].append(p[2])
+
+
+def p_namespace(p):
+    '''namespace : NAMESPACE namespace_scope IDENTIFIER'''
+
+
+def p_namespace_scope(p):
+    '''namespace_scope : '*'
+                       | 'cpp'
+                       | 'java'
+                       | 'py'
+                       | 'perl'
+                       | 'rb'
+                       | 'cocoa'
+                       | 'csharp'
+                       | 'php'
+    '''
+
+
+def p_definition(p):
+    '''definition : const
+                  | typedef
+                  | enum
+                  | struct
+                  | union
+                  | exception
+                  | service
+    '''
+
+
+def p_const(p):
+    '''const : CONST field_type IDENTIFIER '=' const_value '''
+
+
+def p_const_value(p):
+    '''const_value : INTCONSTANT
+                   | DUBCONSTANT
+                   | LITERAL
+                   | IDENTIFIER
+                   | const_list
+                   | const_map'''
+
+
+def p_const_list(p):
+    '''const_list : '[' const_value_sea ']' '''
+
+
+def p_const_value_seq(p):
+    '''const_value_seq : const_value_seq ',' const_value
+                       | const_value_seq ','
+                       | const_value
+                       |'''
+
+
+def p_const_map(p):
+    '''const_map : '{' const_map_seq '}' '''
+
+
+def p_const_map_items(p):
+    '''const_map_seq : const_map_seq ',' const_map_item
+                     | const_map_seq ','
+                     | const_map_item
+                     |'''
+
+
+def p_const_map_item(p):
+    '''const_map_item : '\'' const_value '\'' ':' '\'' const_value '\'' '''
+
+
+def p_typedef(p):
+    '''typedef : TYPEDEF definition_type IDENTIFIER'''
+
+
+def p_enum(p):
+    '''enum : ENUM IDENTIFIER '{' enum_seq '}' '''
+
+
+def p_enum_seq(p):
+    '''enum_seq : enum_seq ',' enum_item
+                | enum_seq ','
+                | enum_item
+                |'''
+
+
+def p_enum_item(p):
+    '''enum_item : IDENTIFIER '=' INTCONSTANT'''
+
+
+def p_struct(p):
+    '''struct : STRUCT IDENTIFIER '{' field_seq '}' '''
+
+
+def p_union(p):
+    '''union : UNION IDENTIFIER '{' field_seq '}' '''
+
+
+def p_exception(p):
+    '''exception : EXCEPTION IDENTIFIER '{' field_seq '}' '''
+
+
+def p_service(p):
+    '''service : SERVICE IDENTIFIER '{' function_seq '}'
+               | SERVICE IDENTIFIER EXTENDS IDENTIFIER '{' function_seq '}'
+    '''
+
+
+def p_function(p):
+    '''function : ONEWAY function_type IDENTIFIER '(' field_seq ')' throws
+                | function_type IDENTIFIER '(' field_seq ')' throws
+                | function_type IDENTIFIER '(' field_seq ')'
+    '''
+
+
+def p_throws(p):
+    '''throws : THROWS '(' field_seq ')' '''
+
+
+def p_function_type(p):
+    '''function_type : field_type | VOID '''
+
+
+def p_field_seq(p):
+    '''field_seq : field_seq ',' field
+                 | field_seq ','
+                 | field
+                 |'''
+
+
+def p_field(p):
+    '''field : field_id field_req field_type IDENTIFIER'''
+
+
+def p_field_id(p):
+    '''field_id : INTCONSTANT ':' '''
+
+
+def p_field_req(p):
+    '''field_req : REQUIRED
+                 | OPTIONAL'''
 
 
 def p_field_type(p):
-    '''field_type : base_type
-                  | container_type
-                  | IDENTIFIER'''
-    p[0] = p[1]
+    '''field_type : IDENTIFIER
+                  | base_type
+                  | container_type'''
 
 
 def p_base_type(p):
-    '''base_type : VOID
-                 | BOOL
+    '''base_type : BOOL
                  | BYTE
                  | I16
                  | I32
@@ -88,70 +187,24 @@ def p_base_type(p):
                  | DOUBLE
                  | STRING
                  | BINARY'''
-    p[0] = p[1]
 
 
 def p_container_type(p):
     '''container_type : map_type
                       | list_type
                       | set_type'''
-    p[0] = p[1]
 
 
 def p_map_type(p):
     '''map_type : MAP '<' field_type ',' field_type '>' '''
-    p[0] = ['map', p[3], p[5]]
 
 
 def p_list_type(p):
     '''list_type : LIST '<' field_type '>' '''
-    p[0] = ['list', p[3]]
 
 
 def p_set_type(p):
     '''set_type : SET '<' field_type '>' '''
-    p[0] = ['set', p[3]]
-
-
-def p_const(p):
-    '''const : CONST field_type IDENTIFIER '=' const_value'''
-    tp = p[2]
-    key = p[3]
-    val = p[5]
-    dct['consts'].append([key, cast(val, tp)])
-
-
-def p_const_value(p):
-    '''const_value : BOOLCONSTANT
-                   | INTCONSTANT
-                   | DUBCONSTANT
-                   | LITERAL
-                   | const_list'''
-    p[0] = p[1]
-
-
-def p_const_list(p):
-    '''const_list : '[' const_sequence ']' '''
-    p[0] = p[2]
-
-def p_const_sequence(p):
-    '''const_sequence : const_sequence ',' const_value
-                      | const_sequence  ','
-                      | const_value
-                      |'''
-    length = len(p)
-
-    if length == 1:
-        p[0] = []
-    elif len(p) == 2:
-        p[0] = [p[1]]
-    elif len(p) == 3:
-        p[0] = p[1]
-    elif len(p) == 4:
-        p[0] = p[1] + [p[3]]
-
-def p_typedef(p):
-    '''typedef : TYPEDEF definition_type IDENTIFIER'''
 
 
 def p_definition_type(p):
@@ -159,4 +212,4 @@ def p_definition_type(p):
                        | container_type'''
 
 
-parser = yacc.yacc(debug=1, write_tables=0)
+parser = yacc.yacc(debug=0, write_tables=0)
